90e01e76eae90d29993a317dde4b9bb078c4b9b6
APfeifer <apfeifer@ubuntu.ubuntu-domain>
2014-02-25 16:45:44 -0500

    Even closer...

--- OCV_Kernel/compile_OCV.sh
+++ OCV_Kernel/compile_OCV.sh
@@ -1,1 +1,1 @@
-g++ *.cpp -lopencv_core -lopencv_highgui -lopencv_objdetect -lopencv_video -lopencv_features2d -lopencv_imgproc -o OCV_Kernel
+g++ *.cpp -fpermissive -lopencv_core -lopencv_highgui -lopencv_objdetect -lopencv_video -lopencv_features2d -lopencv_imgproc -o OCV_Kernel
--- OCV_Kernel/main.cpp
+++ OCV_Kernel/main.cpp
@@ -1,7 +1,9 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
+#include <stdio.h>
 
+using namespace std;
 using namespace cv;
 
 int main()
@@ -11,29 +13,52 @@ int main()
 	{
 		Mat frame, frame_gray; //2 frame buffers
 	
-		frame = imread("eye2.jpg"); //load the frame image
+		frame = imread("eye.jpg"); //load the frame image
 
-		resize(frame, frame, Size(), .25, .25);
-		//resize(frame, frame, Size(), 3, 3);
+		//resize(frame, frame, Size(), .25, .25);
+		resize(frame, frame, Size(), 3, 3);
 
 
 		//Switch to HSV view
 		cvtColor(frame, frame_gray, CV_BGR2HSV);
-		//Smooth the image and 
+		//Smooth the image 
 		GaussianBlur(frame_gray, frame_gray, Size(9,9), 2, 2);
-		//threshold(frame_gray, frame_gray, 20, 255, 1);
-		inRangeS();
+		
+		Mat brown_mask;
+
+		//Mask out everything except the blackest parts of the image
+		inRange(frame_gray, Scalar(0, 0,0), Scalar(255,255,25), brown_mask);
+
+
+		vector<Vec3f> circles;
+		HoughCircles(brown_mask, circles, CV_HOUGH_GRADIENT, 1, 1, 200, 100, 0, 0);
+		cout << circles.size() << endl;
+		for(int j = 0; j < circles.size(); j++)
+		{
+			Point center(cvRound(circles[j][0]), cvRound(circles[j][1]));
+			int radius = cvRound(circles[j][2]);
+
+			circle(frame, center, 3, Scalar(0,255,0), -1, 8, 0);
+			circle(frame, center, radius, Scalar(70,255,0), 3, 8, 0);	
+
+		}
+ 	
+	
+
+
 
 		namedWindow("W1");
 		imshow("W1", frame);
 		namedWindow("W2");
-		imshow("W2", frame_gray);
+		imshow("W2", brown_mask);
+		
 
 
 		waitKey(0);
 
 		destroyWindow("W1");
 		destroyWindow("W2");
+		
 	}
 
 
