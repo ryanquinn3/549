c40275ff99d43020181f8dac85d9d7b3c0cb9447
APfeifer <apfeifer@ubuntu.ubuntu-domain>
2014-02-24 20:30:27 -0500

    Basic Image Display

---
+++ OCV_Kernel/compile_OCV.sh
@@ -0,0 +1,1 @@
+g++ *.cpp -lopencv_core -lopencv_highgui -o OCV_Kernel
--- OCV_Kernel/main.cpp
+++ OCV_Kernel/main.cpp
@@ -1,1 +1,20 @@
-#include "
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+
+using namespace cv;
+
+int main()
+{
+	Mat img = imread("eye.jpg");
+
+	namedWindow("W1", CV_WINDOW_AUTOSIZE);
+	imshow("W1", img);
+
+	waitKey(0);
+
+	destroyWindow("W1");
+
+
+
+	return 0;
+}
