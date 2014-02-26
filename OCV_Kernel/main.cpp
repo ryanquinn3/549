5a62f09ca568d12b5c5981655a8d9475467c73d8
APfeifer <apfeifer@ubuntu.ubuntu-domain>
2014-02-26 11:13:25 -0500

    Updated Source

--- OCV_Kernel/main.cpp
+++ OCV_Kernel/main.cpp
@@ -9,7 +9,7 @@ using namespace cv;
 int main()
 {
 
-	VideoCapture cap("../eye_test.h264");
+	VideoCapture cap("../andy_eye_calc_case.h264");
 	if(!cap.isOpened())
 	{
 		cout << "Failed to open video stream" << endl;
@@ -19,13 +19,18 @@ int main()
 	cout << "Streaming video at an estimated " << cap.get(CV_CAP_PROP_FPS) << " FPS" << endl;
 	
 	Mat frame;
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+
+	namedWindow("W1");
+
 	while(cap.read(frame))
 	{
 		Mat frame_HSV; //2 frame buffers
 	
-		//frame = imread("eye.jpg"); //load the frame image
+		//frame = imread("eye2.jpg"); //load the frame image
 
-		resize(frame, frame, Size(), .75, .75);
+		resize(frame, frame, Size(), .5, .5);
 		//resize(frame, frame, Size(), 3, 3);
 
 
@@ -36,31 +41,33 @@ int main()
 		
 		Mat brown_mask;
 		//Mask out everything except the blackest parts of the image
-		inRange(frame_HSV, Scalar(0, 0,0), Scalar(255,255,15), brown_mask);
+		inRange(frame_HSV, Scalar(0, 0,0), Scalar(255,255,25), brown_mask);
 	
 		//Draw the contours for the isolated pupil
-		vector<vector<Point> > contours;
-		vector<Vec4i> hierarchy;
 		findContours(brown_mask, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
 		
 		Mat contour_edges = Mat::zeros(brown_mask.size(), CV_8UC3);
 		vector<double> areas;
 		//Find the largest contour, and assume it bounds the pupil
 		for(int j = 0; j < contours.size(); j++)
+		{
 			areas.push_back(contourArea(contours[j]));
+			drawContours( contour_edges, contours, j, Scalar(255,0,0), 1,8, hierarchy, 0, Point());
+		}
 
-		cout << "Est. pupil area = " << *max_element(areas.begin(), areas.end()) << endl;
-		drawContours( contour_edges, contours, -1, Scalar(255,0,0), -1,8, hierarchy, 0, Point());
+		if(areas.size() != 0)
+			cout << "Est. pupil area = " << *max_element(areas.begin(), areas.end()) << endl;
 		areas.clear();	
+		contours.clear();
+		hierarchy.clear();
 
-		namedWindow("W1");
 		imshow("W1", contour_edges|frame);
 
 		waitKey(0);
 
-		destroyWindow("W1");
 		
 	}
+	destroyWindow("W1");
 
 	return 0;
 }
