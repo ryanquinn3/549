f17b5233c774c2e710d17568f2957f6848852396
Ryan Quinn <ryan3q@gmail.com>
2014-02-26 11:14:57 -0500

    test

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import subprocess
 
 print "Beginning first eye test"
 
-subprocess.Popen('raspistill -t 10000 -tl 2500 -o myimage_%04d.jpg')
-eye_test.run()
+call = subprocess.Popen('raspistill -t 10000 -tl 2500 -o myimage_%04d.jpg')
+#eye_test.run()
 
 print "Test complete!"
