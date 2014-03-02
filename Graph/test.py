f37cd1286efc6f50fd6773378d779c1547e3f582
Brandon Lee <brandon@ubuntu.(none)>
2014-03-02 12:17:22 -0800

    Made it output two separate images

--- Graph/test.py
+++ Graph/test.py
@@ -2,25 +2,28 @@ import file_parse as fp
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 
 print "Reading File"
 
 data = fp.parse_ocv_output('OCV_pupil_measurements.txt')
 x, y, z = data[0], data[1], data[2]
+x_edit = np.asarray(x, float)
+div = 25.
+x = x_edit / div
 
 plt.figure(1)
-plt.subplot(211)
 
 plt.plot(x, y)
 plt.title('Area of pupil over time')
 plt.ylabel('Area')
 plt.xlabel('Frame (1/25th second)')
+plt.savefig('area_graph.png')
 
-plt.subplot(212)
 plt.title('Ratio of pupil over time')
 plt.plot(x, z)
 plt.ylabel('Ratio')
 plt.xlabel('Frame (1/25th second)')
-plt.show()
+plt.savefig("ratio_graph.png")
 
 print "Test complete!"
