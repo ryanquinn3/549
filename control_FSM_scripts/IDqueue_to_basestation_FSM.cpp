d0d3ae8b9edc4a9b7892736e7d29e23161759a7e
root <root@raspberrypi.(none)>
2014-04-08 15:24:45 +0000

    new piui app

--- control_FSM_scripts/IDqueue_to_basestation_FSM.cpp
+++ control_FSM_scripts/IDqueue_to_basestation_FSM.cpp
@@ -175,7 +175,8 @@ void* FSM2(void* arg)
 			video.close();			
 
 			//Either continue with the next element in the queue, or start the cyclic executive again
-			remove(basestation_results_file.c_str());
+			delete_command = "sudo rm " + basestation_results_file;
+			system(delete_command.c_str());
 		}
 
 		//Empty the local queue cache
@@ -184,27 +185,3 @@ void* FSM2(void* arg)
 
 	return NULL;
 }
-
-
-
-
-/*
-#Copy the data over the wifi link to the receiving laptop - obviously, this is for my laptop :)
-echo "Sending video file for image processing..."
-scp -c blowfish -i /home/pi/.ssh/id_rsa $1 done_file_upstream.txt apfeifer@192.168.1.4:/home/apfeifer/549/
-
-#Wait for the text results from the OpenCV processing
-echo "Waiting for OpenCV results..."
-while [ ! -e "done_file_downstream.txt" ]; do
-
-	if [ -e "OCV_pupil_measurements.txt" ]; then
-		recv_size_new=`stat -c%s "OCV_pupil_measurements.txt"`
-		echo -en "\rReceived $recv_size_new bytes"
-	fi
-
-	sleep 1
-done
-
-echo -en "\rReceived $recv_size_new bytes\n"
-rm done_file_downstream.txt
-*/
